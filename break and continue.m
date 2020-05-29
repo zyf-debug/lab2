8230bed335a16f70199bcd8c6432a382f992503e
zyf-debug <Fran_zhang@sjtu.edu.cn>
2020-05-29 13:34:35 +0800

    test updating code

---
+++ break and continue.m
@@ -0,0 +1,27 @@
+%Break and Continue
+%7.Prime or Not
+num = input('please input a integer: ');
+for i=2:num
+    flag = 1;
+    if mod(num,i)==0
+        disp('0')
+        flag = 0;
+        break
+    else if flag~=0
+        disp('1')
+        break
+        end
+    end
+end
+%7.Construction or Enumeration
+n=input('');
+while n>0
+    m=2;
+    while isprime(m*n+1)
+        m=m+1;
+    end
+    disp(m);
+    n=input('');
+end;
+
+        
